a00de76103d6fa97a64d039e3d7d3ed361621bcb
Douglas <62155670+dougvalerio@users.noreply.github.com>
2022-07-22 20:40:48 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
-let lis = document.querySelectorAll('li')
-lis[0].className = ''
+const lis = document.querySelectorAll('li')
+let grade = 0
+
+const content1 = document.querySelector('#content-1')
+const content2 = document.querySelector('#content-2')
+const selectedGrade = document.querySelector('#grade')
+
+lis.forEach((item) => {
+    item.addEventListener('click', ()=> {
+        selectGrade(item)
+    })
+})
+
+function selectGrade(item) {
+    cleanGrade()
+    item.classList.add('active')
+    grade = item.innerHTML
+} 
+
+function cleanGrade() {
+    let lis = document.querySelectorAll('li')
+    lis.forEach((item) => {
+        item.classList.remove('active')
+    })
+}
+
+function submit() {
+
+    if (! grade) {
+        return
+    }
+
+    content1.setAttribute('style', 'display: none')
+    content2.setAttribute('style', 'display: flex')
+
+    selectedGrade.innerHTML = grade
+}
+
